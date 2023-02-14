e640b01c73eaf7f38964dbddab17f99c20aa90cd
SkyMound <crafteurkiller@gmail.com>
2023-02-14 19:27:27 +0100

    Multiplayer snake

--- Grid.py
+++ Grid.py
@@ -11,7 +11,6 @@ class Grid:
         self.XGridSize = sizeX * self.squareSize
         self.YGridSize = sizeY * self.squareSize
         self.snakes = snakes
-
         self.spawnApple()
         self.tickInterval = tickInterval
         self.drawGrid()
@@ -40,25 +39,32 @@ class Grid:
         for snake in self.snakes :
             snake.onTick()
         
+            # Checking apple
             if(snake.x == self.appleX and snake.y == self.appleY) :
-                self.snake.grow()
-                #change score text with tag 'score'
-                self.canvas.itemconfig('score',text='Score: '+str(self.snake.score))
-
+                snake.grow()
+                self.canvas.itemconfig('score',text='Score: '+str(snake.score))
                 self.spawnApple()
                 self.drawApple()
                 
+            # Checking collision with wall
             if(snake.x < 0 or snake.y < 0 or snake.x > self.sizeX or snake.y > self.sizeY) :
                 print('Snake hit a wall')
+                shouldStop |= True 
+
+            # Checking collision with himself
             elif((snake.x,snake.y) in snake.getTailSquares()) :
                 print('Snake hit himself')
+                shouldStop |= True
+
+            # TODO Checking collsion with others
+            
             else :
-                shouldStop = True
+                shouldStop |= False
+
+            self.refreshSnake(snake)
 
         if(not(shouldStop)):
             Timer(self.tickInterval,self.onTick).start()
-
-        self.refreshSnake()
 
         #put score in first plan
         self.canvas.tag_raise('score')
@@ -73,31 +79,29 @@ class Grid:
         self.canvas = Canvas(root,width=self.XGridSize,height=self.YGridSize)
         self.canvas.pack()
         self.canvas.create_rectangle(0,0,self.XGridSize,self.YGridSize,fill='black')
-        self.drawSnake()
+        for snake in self.snakes :
+            self.drawSnake(snake)
         self.drawApple()
         self.drawScore()
-
         Timer(self.tickInterval,self.onTick).start()
         root.mainloop()
 
 
-    def refreshSnake(self) :
+    def refreshSnake(self,snake) :
         
         #erase the last part of the snake via lastremove
-        self.canvas.create_rectangle(self.snake.lastRemoved.x*self.squareSize,self.snake.lastRemoved.y*self.squareSize,self.snake.lastRemoved.x*self.squareSize+self.squareSize,self.snake.lastRemoved.y*self.squareSize+self.squareSize,fill='black')
+        self.canvas.create_rectangle(snake.lastRemoved.x*self.squareSize,snake.lastRemoved.y*self.squareSize,snake.lastRemoved.x*self.squareSize+self.squareSize,snake.lastRemoved.y*self.squareSize+self.squareSize,fill='black')
         #draw the head of the snake
-        self.canvas.create_rectangle(self.snake.x*self.squareSize,self.snake.y*self.squareSize,self.snake.x*self.squareSize+self.squareSize,self.snake.y*self.squareSize+self.squareSize,fill='green')
+        self.canvas.create_rectangle(snake.x*self.squareSize,snake.y*self.squareSize,snake.x*self.squareSize+self.squareSize,snake.y*self.squareSize+self.squareSize,fill='green')
 
-
-
-    def drawSnake(self) :
-        for x,y in self.snake.getOccupiedSquares() :
+    def drawSnake(self,snake) :
+        for x,y in snake.getOccupiedSquares() :
             self.canvas.create_rectangle(x*self.squareSize,y*self.squareSize,x*self.squareSize+self.squareSize,y*self.squareSize+self.squareSize,fill='green')
         
     def drawApple(self) :
         self.canvas.create_rectangle(self.appleX*self.squareSize,self.appleY*self.squareSize,self.appleX*self.squareSize+self.squareSize,self.appleY*self.squareSize+self.squareSize,fill='red')
 
     def drawScore(self) :
-        self.textScore = self.canvas.create_text(50,20,fill='white',font='Times 20',text='Score: '+str(self.snake.score),tag='score',width=200)
+        self.textScore = self.canvas.create_text(50,20,fill='white',font='Times 20',text='Score: '+str(self.snakes[0].score),tag='score',width=200)
         
 
--- main.py
+++ main.py
@@ -5,5 +5,5 @@ gridSizeX = 50
 gridSizeY = 50 
 interval = 0.05
 # Snake(gridSizeX//2,gridSizeY//2,3)
-snakes = [Snake(gridSizeX//2,gridSizeY//2,3),Snake(gridSizeX//3,gridSizeY//3,3)]
+snakes = [Snake(gridSizeX//2,gridSizeY//2,3,'q','d','z','s'),Snake(gridSizeX//3,gridSizeY//3,3,'j','l','i','k')]
 grid = Grid(gridSizeX,gridSizeY,interval,snakes)
