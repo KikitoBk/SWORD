6cd590bdbe52d70e45d8dbc922f5df90f5aba94d
SkyMound <crafteurkiller@gmail.com>
2023-02-15 11:54:42 +0100

    basic setup

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from Snake import Snake
 gridSizeX = 50
 gridSizeY = 50 
 interval = 0.05
-# Snake(gridSizeX//2,gridSizeY//2,3)
-snakes = [Snake(gridSizeX//2,gridSizeY//2,3,'q','d','z','s'),Snake(gridSizeX//3,gridSizeY//3,3,'j','l','i','k')]
+snake1 = Snake(gridSizeX//2,gridSizeY//2,3,'green','q','d','z','s')
+snake2 = Snake(gridSizeX//3,gridSizeY//3,3,'orange','j','l','i','k')
+snakes = [snake1]
 grid = Grid(gridSizeX,gridSizeY,interval,snakes)
